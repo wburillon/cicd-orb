fb56f2628d30c5d6a13d5288e995648c7071c1d9
William Burillon <wburillon@i-tracing.com>
2025-07-21 16:08:12 +0200

    Fix: update litle dummy problem

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -4,6 +4,6 @@ DOCKER_COMPOSE=$(circleci env subst "${DOCKER_COMPOSE_FILE}")
 IMAGE=$(circleci env subst "${IMAGE_NAME}")
 
 
-docker-compose -f $DOCKER_COMPOSE build
-docker save -o $IMAGE.tar $IMAGE
+docker-compose -f "$DOCKER_COMPOSE" build
+docker save -o "$IMAGE".tar "$IMAGE"
 
